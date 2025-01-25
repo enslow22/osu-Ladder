9de7aef260431a67ecce67f4dbdb3550c0c60acd
Ryan Enslow <enslow22@up.edu>
2025-01-24 21:57:20 -0600

    Moved to the webapi package

---
+++ web/frontend/script.js
@@ -0,0 +1,42 @@
+function submit() {
+    alert("hi")
+}
+
+function getCookie() {
+    value_or_null = (document.cookie.match(/^(?:.*;)?\s*session\s*=\s*([^;]+)(?:.*)?$/)||[,null])[1]
+    console.log(value_or_null)
+}
+
+function login() {
+    // Move to login
+    window.location.href = "/login";
+}
+
+async function fetchAsync(url) {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
+
+async function logout() {
+    a = await fetch('/logout', {
+    method: "POST",
+    body: '{}',
+    headers: {
+    'Content-Type': 'application/json'
+    }}).then(() => {window.location.href = "/"});
+}
+
+async function callInitialFetch(apikey) {
+    catch_converts = document.getElementById('catch_converts')
+
+    q_string = '?catch_converts='+catch_converts.value.toString()
+    console.log(q_string)
+    a = await fetch('/initial_fetch_self/' + q_string , {
+    method: "POST",
+    body: '{}',
+    headers: {
+    'Content-Type': 'application/json'
+    }});
+    return a
+}
