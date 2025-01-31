4ecde7bf602ff437b883b8584635c1a3d2653a6d
Ryan Enslow <enslow22@up.edu>
2025-01-30 21:41:26 -0600

    completely new fetchQueue

--- database/fetchQueue.py
+++ database/fetchQueue.py
@@ -1,173 +1,147 @@
+import multiprocessing.pool
+import time
 import datetime
-import os.path
 import queue
-from models import RegisteredUser
-from osuApiAuthService import OsuApiAuthService
-from userService import refresh_tokens
-from scoreService import insert_scores
-import threading
-from sqlalchemy import select
+from database.userService import refresh_tokens, register_user
+from database.osuApiAuthService import OsuApiAuthService
+from database.scoreService import insert_scores
+from database.ORM import ORM
+import os
+import dotenv
 
-# TODO rewrite basically all of this and learn how to use the background tasks functionality in fastapi
-# TODO this function should use the authenticated user's access token and not my own. Then we can fetch multiple people at once
+import sqlalchemy.orm
 
-UPDATE_INTERVAL_SECONDS = 28800
+from database.models import RegisteredUser
+
+
+# A task in this context looks like
+# (time_set: int, RegisteredUser: user, catch_converts: bool)
+
+dotenv.load_dotenv('../database/.env')
+NUM_THREADS = int(os.getenv('NUM_THREADS'))
+
 
 class TaskQueue:
 
-    # The order of this list determines priority order from lowest to highest
-    # Tasks queued with higher priority will always go before tasks with lower priority
-    all_types = ['daily_fetch', 'initial_fetch']
-
-    def __init__(self, sessionmaker):
+    def __init__(self, sessionmaker: sqlalchemy.orm.sessionmaker):
         self.sessionmaker = sessionmaker
         self.q = queue.PriorityQueue()
-        self.current = None
+        self.pool = multiprocessing.pool.ThreadPool(processes=NUM_THREADS)
+        self.current = []
+        # {'user_id': user.user_id,
+        #   'username': user.username,
+        #   'catch_converts': catch_converts,
+        #   'num_maps': num_maps,}
 
-    def enqueue(self, task: tuple[str, dict]):
-        # Verify task first
-        task_type = task[0]
-        data = task[1]
-
-        if task_type not in self.all_types:
-            return
+    def enqueue(self, user_id: int, catch_converts: bool) -> bool:
         session = self.sessionmaker()
-        user_queue = [x[3]['user_id'] for x in self.q.queue]
-        bonus_priority = 0
-        if task_type == 'daily_fetch':
-            import datetime
-            user = session.get(RegisteredUser, data['user_id'])
-            session.close()
-            if user is None:
-                print('User %s not registered' % data['user_id'])
-                return
-            if user.last_updated is None:
-                print('Please do an initial fetch for %s first' % user.username)
-                return
-            if user in self.q.queue:
-                print('%s is already in the queue!' % user.username)
-                return
-            print('Adding %s to the daily fetch queue' % user.username)
-        elif task_type == 'initial_fetch':
-            user = session.get(RegisteredUser, data['user_id'])
-            session.close()
-            bonus_priority = 1 if data['catch_converts'] else 0
-            if user is None:
-                print('User %s not registered' % data['user_id'])
-                return
-            if user.last_updated is not None:
-                print('User %s does not need to be fetched.' % user.username)
-                return
-            if self.current is not None:
-                if self.current[1]['user_id'] == user.user_id:
-                    print('%s is already in the queue!' % user.username)
-                    return
-            if user.user_id in user_queue:
-                print('%s is already in the queue!' % user.username)
-                return
-            catch_string = ' Also fetching catch converts.' if data['catch_converts'] else ''
-            print('Adding %s to the fetch queue.%s'% (user.username, catch_string))
-        import time
-        self.q.put((self.all_types.index(task_type)+1, time.time()+bonus_priority*3600, task_type, data))
-        if self.current is None:
+        user = session.get(RegisteredUser, user_id)
+        session.close()
+
+        user_queue = [x[1].user_id for x in self.q.queue]
+        bonus_priority = 1 if catch_converts else 0
+
+        if user is None:
+            return False
+        if user.last_updated is not None:
+            return False
+        if user.access_token is None or user.refresh_token is None or user.expires_at is None:
+            return False
+        for tasks in self.current:
+            if tasks['user_id'] == user.user_id:
+                return False
+        if user.user_id in user_queue:
+            return False
+        catch_string = ' Also fetching catch converts.' if catch_converts else ''
+        print('Adding %s to the fetch queue.%s' % (user.username, catch_string))
+        self.q.put((time.time()+bonus_priority*7200, user, catch_converts))
+        if len(self.current) < NUM_THREADS and not self.q.empty():
             self.start()
-        return
+        return True
 
     def start(self):
-        prio, time_enqueued, task_type, data = self.q.get()
-        self.current = (task_type, data)
-        t = threading.Thread(target=self.process)
-        t.start()
-
-    def process(self):
-        # Based on the type of task in self.current, do something
-        task_type = self.current[0]
-        data = self.current[1]
-        session = self.sessionmaker()
-
-        if task_type == 'daily_fetch':
-            user = session.get(RegisteredUser, data['user_id'])
-            print('Starting daily_fetch for %s' % user.username)
-            try:
-                print('Finished fetching %s\'s daily scores.\n' % user.username)
-                user.last_updated = datetime.datetime.now()
-            except Exception as e:
-                print(e)
-        elif task_type == 'initial_fetch':
-            user = session.get(RegisteredUser, data['user_id'])
-            catch_converts = data['catch_converts']
-            catch_string = ' Also fetching catch converts.' if catch_converts else ''
-            modes = ('osu', 'fruits') if catch_converts else ('osu',)
-
-            print('Starting initial_fetch for %s' % user.username)
-
-            # Check that access token is not expired
-            if user.expires_at < datetime.datetime.now():
-                refresh_tokens(session, user)
-
-            auth_osu_api = OsuApiAuthService(user.user_id, user.access_token)
-
-            most_played = auth_osu_api.get_all_played_maps()
-            most_played = [{'beatmap_id': x.beatmap_id,
-                            'beatmapset_id': x.beatmapset.id,
-                            'status': x.beatmapset.status.value} for x in most_played]
-            most_played = list(filter(lambda x: x['status'] in [1, 2, 4], most_played))
-            num_maps = len(most_played)
-            print('Beginning fetch for %s!%s They have %s maps in their most played.' % (user.username, catch_string, str(len(most_played))))
-
-            try:
-                # For all maps, fetch the user's score on that map
-                while len(most_played) > 0:
-                    count = num_maps - len(most_played)
-                    if count % 50 == 0:
-                        print('Added %s maps for %s so far!' % (str(count), user.username))
-                    beatmap = most_played.pop()
-                    for mode in modes:
-                        new_scores = auth_osu_api.get_user_scores_on_map(beatmap['beatmap_id'], mode=mode)
-                        insert_scores(self.sessionmaker(), new_scores)
-                user.last_updated = datetime.datetime.now()
-            except Exception as e:
-                # Also add a flag to know if a user was kicked out in the middle of their initial fetch
-                import pickle
-                path = os.path.join(os.getcwd(), 'pickle')
-                os.makedirs(path, exist_ok=True)
-                with open('pickle/%s.pkl' % user.username, 'wb') as f:
-                    pickle.dump(self.current, f)
-                print(e)
-
-        session.commit()
-        self.current = None
-        if not self.q.empty():
+        time_set, user, catch_converts = self.q.get()
+        self.current.append({'user_id': user.user_id,
+                             'username': user.username,
+                             'catch_converts': catch_converts,
+                             'num_maps': float('nan')})
+        self.pool.apply_async(self.process, args=(user, catch_converts))
+        #self.pool.apply_async(self.test, args=(user, catch_converts))
+        if len(self.current) < NUM_THREADS and not self.q.empty():
             self.start()
 
-    def get_priority(self, task_type: str):
-        return self.all_types.index(task_type)
+    def process(self, user: RegisteredUser, catch_converts: bool):
+        for task in self.current:
+            if task['user_id'] == user.user_id:
+                task['num_maps'] = 'Calculating'
 
-    def daily_queue_all(self, force: bool = False):
-        from datetime import timedelta
-        today = datetime.datetime.now()
-        time_diff = today - timedelta(seconds=UPDATE_INTERVAL_SECONDS)
+        catch_string = ' Also fetching catch converts.' if catch_converts else ''
+        print('Starting initial_fetch for %s.%s' % (user.username, catch_string))
+
         session = self.sessionmaker()
-        stmt = select(RegisteredUser)
-        if not force:
-            stmt = stmt.filter(RegisteredUser.last_updated < time_diff)
-        all_users = session.scalars(stmt).all()
-        session.close()
-        for user in all_users:
-            self.enqueue(('daily_fetch', {'user_id': user.user_id}))
+        if user.expires_at < datetime.datetime.now():
+            refresh_tokens(session, user)
+        auth_osu_api = OsuApiAuthService(user.user_id, user.access_token)
+
+        print('%s accessed the osu api with access_token %s' % (user.username, user.access_token))
+
+        most_played = auth_osu_api.get_all_played_maps()
+        most_played = [{'beatmap_id': x.beatmap_id,
+                        'beatmapset_id': x.beatmapset.id,
+                        'mode': x._beatmap.mode.value,
+                        'status': x.beatmapset.status.value} for x in most_played]
+        most_played = list(filter(lambda x: x['status'] in [1, 2, 4], most_played))
+
+        for task in self.current:
+            if task['user_id'] == user.user_id:
+                task['num_maps'] = len(most_played)
+
+
+        print('Beginning fetch for %s!%s They have %s maps in their most played.' % (
+        user.username, catch_string, str(len(most_played))))
+        try:
+
+            # For all maps, fetch the user's score on that map
+            while len(most_played) > 0:
+                beatmap = most_played.pop()
+
+                # Get the default mode score first
+                new_scores = auth_osu_api.get_user_scores_on_map(beatmap['beatmap_id'])
+
+                # If the map has converts and the user wants converts, then get those as well.
+                if catch_converts and beatmap['mode'] == 'osu':
+                    new_scores += auth_osu_api.get_user_scores_on_map(beatmap['beatmap_id'], mode='fruits')
+                insert_scores(self.sessionmaker(), new_scores)
+                for task in self.current:
+                    if task['user_id'] == user.user_id:
+                        task['num_maps'] = len(most_played)
+
+            user.last_updated = datetime.datetime.now()
+            for task in self.current:
+                if task['user_id'] == user.user_id:
+                    self.current.remove(task)
+            self.start()
+        except Exception as e:
+            # Also add a flag to know if a user was kicked out in the middle of their initial fetch
+            import pickle
+            import os
+            path = os.path.join(os.getcwd(), 'pickle')
+            os.makedirs(path, exist_ok=True)
+            with open('pickle/%s.pkl' % user.username, 'wb') as f:
+                pickle.dump(self.current, f)
+            print(e)
 
 if __name__ == '__main__':
-    pass
-    """
-    score_service = ScoreService(orm.sessionmaker())
-    fq = FetchQueue(orm.sessionmaker)
-    dq = DailyQueue(orm.sessionmaker)
-    dq.enqueue_all()
+    orm = ORM()
+    tq = TaskQueue(orm.sessionmaker)
+    session = orm.sessionmaker()
 
-    test_data = [(20720260, ('mania',)),
-                 (20651411, ('taiko',))]
-    fq.enqueue( 20651412, ('osu',) )
-    for d in test_data:
-        fq.enqueue(*d)
-    print(fq.q.queue)
-    """
+    tq.enqueue(4991273, False)
+    tq.enqueue(10651409, False)
+    #tq.enqueue(3634152, False)
+
+    while True:
+        print(tq.current)
+        print(tq.q.queue)
+        print('\n')
+        time.sleep(5)
