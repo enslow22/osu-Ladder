962ab05d338b36f720f8a90bce6b194b8424ca68
Ryan Enslow <enslow22@up.edu>
2025-02-03 17:49:27 -0600

    fetch displays on home page now

--- web/frontend/script.js
+++ web/frontend/script.js
@@ -27,16 +27,48 @@ async function logout() {
     }}).then(() => {window.location.href = "/"});
 }
 
-async function callInitialFetch(apikey) {
+async function callInitialFetch() {
     catch_converts = document.getElementById('catch_converts')
 
     q_string = '?catch_converts='+catch_converts.checked.toString()
-    console.log(q_string)
     a = await fetch('/initial_fetch_self' + q_string , {
     method: "POST",
     body: '{}',
     headers: {
     'Content-Type': 'application/json'
-    }});
-    return a
+    }}).then(res => res.json());
 }
+
+async function fetchQueue() {
+    const response = await fetch('/fetch_queue');
+    const data = await response.json();
+    current = data['current']
+    queue = data['in queue']
+    //queue = [{"username":"aaa", "user_id": 1111111, "catch_converts": true},{"username":"aaa", "user_id": 1111111, "catch_converts": true},{"username":"aaa", "user_id": 1111111, "catch_converts": true},{"username":"aaa", "user_id": 1111111, "catch_converts": true},{"username":"aaa", "user_id": 1111111, "catch_converts": true},{"username":"aaa", "user_id": 1111111, "catch_converts": true},{"username":"aaa", "user_id": 1111111, "catch_converts": true},{"username":"aaa", "user_id": 1111111, "catch_converts": true},{"username":"aaa", "user_id": 1111111, "catch_converts": true}]
+    fetch_queue = document.getElementById('fetch_queue_container')
+
+    headers = '<tr><th>User Id</th><th>Username</th><th>Catch Converts?</th><th>Maps Left</th></tr>'
+
+    queue_list = []
+
+    for (let i = 0; i < current.length; i++) {
+        queue_list.push('<tr>'+
+        '<td>'+String(current[i].username)+'</td>'+
+        '<td>'+String(current[i].user_id)+'</td>'+
+        '<td>'+String(current[i].catch_converts)+'</td>'+
+        '<td>'+String(current[i].num_maps)+'</td></tr>')
+    }
+
+    for (let i = 0; i < queue.length; i++) {
+        queue_list.push('<tr>'+
+        '<td>'+String(queue[i].username)+'</td>'+
+        '<td>'+String(queue[i].user_id)+'</td>'+
+        '<td>'+String(queue[i].catch_converts)+'</td><td> </td></tr>')
+    }
+
+    // console.log(queue_list)
+    console.log(headers + String(queue_list))
+    fetch_queue.innerHTML = headers + queue_list.join('')
+}
+
+fetchQueue()
