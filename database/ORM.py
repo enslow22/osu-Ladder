b16f4cadcdd98bca7cf32ca61e1125729ef63ac1
Ryan Enslow <enslow22@up.edu>
2025-02-23 14:04:47 -0600

    Added a fallback for initializing a db connection from outside a container

--- database/ORM.py
+++ database/ORM.py
@@ -1,4 +1,6 @@
 import os
+
+import sqlalchemy.exc
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
@@ -13,7 +15,11 @@ class ORM:
         port = os.getenv('DB_PORT')
         dbname = os.getenv('DB_NAME')
         connection_string = "mysql+mysqldb://%s:%s@%s:%s/%s" % (user, password, host, port, dbname)
-        self.engine = create_engine(connection_string, echo=False)
+        try:
+            self.engine = create_engine(connection_string, echo=False)
+        except sqlalchemy.exc.OperationalError:
+            connection_string = "mysql+mysqldb://%s:%s@%s:%s/%s" % (user, password, 'localhost', port, dbname)
+            self.engine = create_engine(connection_string, echo=False)
         self.sessionmaker = sessionmaker(self.engine)
         self.session = self.sessionmaker()
 
